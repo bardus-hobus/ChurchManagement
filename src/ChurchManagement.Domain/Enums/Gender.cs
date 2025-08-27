91b2cbe5321ffd79c174b4912eaed36f2328ea5e
Daniel Cory <daniel.cory@centralsquare.com>
2025-08-26 23:22:37 -0400

    Fixed Gender to only allow Male and Female.

--- src/ChurchManagement.Domain/Enums/Gender.cs
+++ src/ChurchManagement.Domain/Enums/Gender.cs
@@ -2,8 +2,7 @@ namespace ChurchManagement.Domain.Enums;
 
 public enum Gender
 {
+    //So God created man in his own image, in the image of God created he him; male and female created he them. Gen 1:27
     Male,
-    Female,
-    Other,
-    PreferNotToSay
+    Female
 }
